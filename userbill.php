2e338b277791eaef6811fe6bcd08c3c320c5afb1
JMJ Riyana <jeemariyana@gmail.com>
2020-04-07 21:28:06 +0530

    code

--- userbill.php
+++ userbill.php
@@ -32,110 +32,91 @@
                 <div class="card-body">
                     <form method="post" id="invoice_form">
                         <div class="table-responsive">
-                            <table class="table table-bordered">
+                            <table class="table table-bordered table-hover" id="invoiceItem">
                                 <tr>
-                                    <td colspan="2">
-                                        <div class="row">
-                                            <div class="col-md-4">
-
-                                                <b>RECEIVER (BILL TO)</b><br />
-                                                <input type="text" name="order_receiver_name" id="order_receiver_id"
-                                                    class="form-control input-sm" placeholder="Enter Reciever ID" />
-
-                                            </div>
-                                            <div class="col-md-4">
-
-                                                <b>Bill Details</b><br />
-                                                <input type="text" name="order_no" id="order_no"
-                                                    class="form-control input-sm" readonly placeholder="Bill No" />
-                                            </div>
-                                            <div class="col-md-4">
-                                                <b>Date</b><br />
-
-                                                <label id="order_date"> </label>
-
-                                            </div>
-                                        </div>
-                                        <br />
-                                        <table id="invoice-item-table" class="table table-bordered">
-                                            <tr>
-                                                <th width="6%">Sr No.</th>
-                                                <th width="18%">Item Code</th>
-                                                <th width="20%">Item Name</th>
-                                                <th width="4%">Quantity</th>
-                                                <th width="10%">Price</th>
-                                                <th width="11%">Amount</th>
-
-
-                                            </tr>
-
-                                            <tr>
-                                                <td><span id="sr_no">1</span></td>
-                                                <td><input type="text" name="item_code[]" id="item_code1"
-                                                        class="form-control input-sm" /></td>
-                                                <td><input type="text" name="item_name[]" id="item_name1"
-                                                        class="form-control input-sm" readonly /></td>
-
-                                                <td><input type="text" name="order_item_quantity[]"
-                                                        id="order_item_quantity1" data-srno="1"
-                                                        class="form-control input-sm order_item_quantity" /></td>
-                                                <td><input type="text" name="order_item_price[]" id="order_item_price1"
-                                                        class="form-control input-sm" readonly /></td>
-                                                <td><input type="text" name="order_item_amount[]"
-                                                        id="order_item__amount1" data-srno="1"
-                                                        class="form-control input-sm order_item_actual_amount"
-                                                        readonly /></td>
-
-                                            </tr>
-
-                                        </table>
-                                        <div align="right">
-                                            <button type="button" name="add_row" id="add_row"
-                                                class="btn btn-success btn-xs">+</button>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td width="64%" align="right" style="color:red"><b>Discount (%)</td>
-                                    <td><input type="text" name="item_discount[]" id="item_discount1"
-                                            class="form-control input-sm" /></td>
+                                    <th width="2%"><input id="checkAll" class="formcontrol" type="checkbox"></th>
+                                    <th width="15%">Item No</th>
+                                    <th width="38%">Item Name</th>
+                                    <th width="15%">Quantity</th>
+                                    <th width="15%">Price</th>
+                                    <th width="15%">Total</th>
                                 </tr>
 
                                 <tr>
-                                    <td align="right" style="color:darkblue"><b>Total</td>
-                                    <td align="right"><b><span id="final_total_amt"></span></b></td>
+                                    <td><input class="itemRow" type="checkbox"></td>
+                                    <td><input type="text" value="<?php echo $invoiceItem["item_code"]; ?>"
+                                            name="productCode[]" id="productCode_<?php echo $count; ?>"
+                                            class="form-control" autocomplete="off"></td>
+                                    <td><input type="text" value="<?php echo $invoiceItem["item_name"]; ?>"
+                                            name="productName[]" id="productName_<?php echo $count; ?>"
+                                            class="form-control" autocomplete="off"></td>
+                                    <td><input type="number" value="<?php echo $invoiceItem["order_item_quantity"]; ?>"
+                                            name="quantity[]" id="quantity_<?php echo $count; ?>"
+                                            class="form-control quantity" autocomplete="off"></td>
+                                    <td><input type="number" value="<?php echo $invoiceItem["order_item_price"]; ?>"
+                                            name="price[]" id="price_<?php echo $count; ?>" class="form-control price"
+                                            autocomplete="off"></td>
+                                    <td><input type="number"
+                                            value="<?php echo $invoiceItem["order_item_final_amount"]; ?>"
+                                            name="total[]" id="total_<?php echo $count; ?>" class="form-control total"
+                                            autocomplete="off"></td>
+                                    <input type="hidden" value="<?php echo $invoiceItem['order_item_id']; ?>"
+                                        class="form-control" name="itemId[]">
                                 </tr>
-                                <tr>
-                                    <td colspan="2"></td>
-                                </tr>
-
-
-                                <tr>
-                                    <td>
-                                        <textarea name="order_notes" id="order_notes" class="form-control"
-                                            placeholder="Additional Notes..."></textarea>
-
-                                    </td>
-                                    <td colspan="3" align="center">
-                                        <input type="hidden" name="total_item" id="total_item" value="1" />
-                                        <input type="submit" name="create_ebill" id="create_ebill"
-                                            class="btn btn-secondary btn-sm" value="E-Bill" />
-                                        <input type="button" name="print" id="print" class="btn btn-info btn-sm"
-                                            value="Print" />
-                                        <input type="button" name="text_msg" id="text_msg"
-                                            class="btn btn-success btn-sm" value="Text" />
-                                        <input type="reset" name="cancel" id="cancel" class="btn btn-danger btn-sm"
-                                            value="Cancel" />
-                                    </td>
-                                </tr>
+                                <?php } ?>
                             </table>
                         </div>
-                    </form>
-                    </p>
-
                 </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                        <button class="btn btn-danger delete" id="removeRows" type="button">- Delete</button>
+                        <button class="btn btn-success" id="addRows" type="button">+ Add More</button>
+                    </div>
+                </div>
+                <div align="right">
+                    <button type="button" name="add_row" id="add_row" class="btn btn-success btn-xs">+</button>
+                </div>
+                </td>
+                </tr>
+                <tr>
+                    <td width="64%" align="right" style="color:red"><b>Discount (%)</td>
+                    <td><input type="text" name="item_discount[]" id="item_discount1" class="form-control input-sm" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align="right" style="color:darkblue"><b>Total</td>
+                    <td align="right"><b><span id="final_total_amt"></span></b></td>
+                </tr>
+                <tr>
+                    <td colspan="2"></td>
+                </tr>
+
+
+                <tr>
+                    <td>
+                        <textarea name="order_notes" id="order_notes" class="form-control"
+                            placeholder="Additional Notes..."></textarea>
+
+                    </td>
+                    <td colspan="3" align="center">
+                        <input type="hidden" name="total_item" id="total_item" value="1" />
+                        <input type="submit" name="create_ebill" id="create_ebill" class="btn btn-secondary btn-sm"
+                            value="E-Bill" />
+                        <input type="button" name="print" id="print" class="btn btn-info btn-sm" value="Print" />
+                        <input type="button" name="text_msg" id="text_msg" class="btn btn-success btn-sm"
+                            value="Text" />
+                        <input type="reset" name="cancel" id="cancel" class="btn btn-danger btn-sm" value="Cancel" />
+                    </td>
+                </tr>
+                </table>
             </div>
+            </form>
+            </p>
+
         </div>
+    </div>
+    </div>
     </div>
 
     <script>
