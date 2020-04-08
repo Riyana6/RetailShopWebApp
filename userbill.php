bc135820f4f1decfca1311a9083026e07c8b4f6e
JMJ Riyana <jeemariyana@gmail.com>
2020-04-08 20:00:19 +0530

    code

--- userbill.php
+++ userbill.php
@@ -66,114 +66,69 @@
                                 <?php ?>
                             </table>
                         </div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                        <button class="btn btn-danger delete" id="removeRows" type="button">- Delete</button>
-                        <button class="btn btn-success" id="addRows" type="button">+ Add More</button>
+                    </form>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                            <button class="btn btn-danger delete" id="removeRows" type="button">- Delete</button>
+                            <button class="btn btn-success" id="addRows" type="button">+ Add More</button>
+                        </div>
                     </div>
+                    <div align="right">
+                        <button type="button" name="add_row" id="add_row" class="btn btn-success btn-xs">+</button>
+                    </div>
+                    <tr>
+                        <td>
+                            <textarea name="order_notes" id="order_notes" class="form-control"
+                                placeholder="Additional Notes... "></textarea>
+
+                        </td>
+                        <td colspan="3" align="center">
+                            <input type="hidden" name="total_item" id="total_item" value="1" />
+                            <input type="submit" name="create_ebill" id="create_ebill" class="btn btn-secondary btn-sm"
+                                value="E-Bill" />
+                            <input type="button" name="print" id="print" class="btn btn-info btn-sm" value="Print" />
+                            <input type="button" name="text_msg" id="text_msg" class="btn btn-success btn-sm"
+                                value="Text" />
+                            <input type="reset" name="cancel" id="cancel" class="btn btn-danger btn-sm"
+                                value="Cancel" />
+                        </td>
+                    </tr>
+                    </table>
                 </div>
-                <div align="right">
-                    <button type="button" name="add_row" id="add_row" class="btn btn-success btn-xs">+</button>
-                </div>
-                </td>
-                </tr>
-                <tr>
-                    <td width="64%" align="right" style="color:red"><b>Discount (%)</td>
-                    <td><input type="text" name="item_discount[]" id="item_discount1" class="form-control input-sm" />
+
+                <table>
+                    <td align="right">
+                        <tr><label>Subtotal: </label><input
+                                value="<?php echo $invoiceValues['order_total_before_tax']; ?>" type="number"
+                                class="form-control" name="subTotal" id="subTotal" placeholder="Subtotal">
+                        </tr>
+                        <tr><label>Discount: </label><input value="<?php echo $invoiceValues['order_total_tax']; ?>"
+                                type="number" class="form-control" name="taxAmount" id="taxAmount"
+                                placeholder="Tax Amount">
+                        </tr>
+                        <tr><label>Total: </label><input value="<?php echo $invoiceValues['order_total_after_tax']; ?>"
+                                type="number" class="form-control" name="totalAftertax" id="totalAftertax"
+                                placeholder="Total">
+                        </tr>
+                        <tr><label>Amount Paid: </label><input
+                                value="<?php echo $invoiceValues['order_amount_paid']; ?>" type="number"
+                                class="form-control" name="amountPaid" id="amountPaid" placeholder="Amount Paid">
+                        </tr>
+                        <tr>
+                        </tr>
                     </td>
-                </tr>
-
-                <tr>
-                    <td align="right" style="color:darkblue"><b>Total</td>
-                    <td align="right"><b><span id="final_total_amt"></span></b></td>
-                </tr>
-                <tr>
-                    <td colspan="2"></td>
-                </tr>
-
-
-                <tr>
-                    <td>
-                        <textarea name="order_notes" id="order_notes" class="form-control"
-                            placeholder="Additional Notes... "></textarea>
-
-                    </td>
-                    <td colspan="3" align="center">
-                        <input type="hidden" name="total_item" id="total_item" value="1" />
-                        <input type="submit" name="create_ebill" id="create_ebill" class="btn btn-secondary btn-sm"
-                            value="E-Bill" />
-                        <input type="button" name="print" id="print" class="btn btn-info btn-sm" value="Print" />
-                        <input type="button" name="text_msg" id="text_msg" class="btn btn-success btn-sm"
-                            value="Text" />
-                        <input type="reset" name="cancel" id="cancel" class="btn btn-danger btn-sm" value="Cancel" />
-                    </td>
-                </tr>
                 </table>
             </div>
-            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                <span class="form-inline">
-                    <div class="form-group">
-                        <label>Subtotal: </label>
-                        <div class="input-group">
-                            <div class="input-group-addon currency">$</div>
-                            <input value="<?php echo $invoiceValues['order_total_before_tax']; ?>" type="number"
-                                class="form-control" name="subTotal" id="subTotal" placeholder="Subtotal">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label>Tax Rate: </label>
-                        <div class="input-group">
-                            <input value="<?php echo $invoiceValues['order_tax_per']; ?>" type="number"
-                                class="form-control" name="taxRate" id="taxRate" placeholder="Tax Rate">
-                            <div class="input-group-addon">%</div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label>Tax Amount: </label>
-                        <div class="input-group">
-                            <div class="input-group-addon currency">$</div>
-                            <input value="<?php echo $invoiceValues['order_total_tax']; ?>" type="number"
-                                class="form-control" name="taxAmount" id="taxAmount" placeholder="Tax Amount">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label>Total: </label>
-                        <div class="input-group">
-                            <div class="input-group-addon currency">$</div>
-                            <input value="<?php echo $invoiceValues['order_total_after_tax']; ?>" type="number"
-                                class="form-control" name="totalAftertax" id="totalAftertax" placeholder="Total">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label>Amount Paid: </label>
-                        <div class="input-group">
-                            <div class="input-group-addon currency">$</div>
-                            <input value="<?php echo $invoiceValues['order_amount_paid']; ?>" type="number"
-                                class="form-control" name="amountPaid" id="amountPaid" placeholder="Amount Paid">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label>Amount Due: </label>
-                        <div class="input-group">
-                            <div class="input-group-addon currency">$</div>
-                            <input value="<?php echo $invoiceValues['order_total_amount_due']; ?>" type="number"
-                                class="form-control" name="amountDue" id="amountDue" placeholder="Amount Due">
-                        </div>
-                    </div>
-                </span>
-            </div>
+
+            </p>
+
         </div>
-        </form>
-        </p>
-
-    </div>
 
 
-    <script>
-    var d = new Date();
-    document.getElementById("order_date").innerHTML = d;
-    </script>
+        <script>
+        var d = new Date();
+        document.getElementById("order_date").innerHTML = d;
+        </script>
 
 </body>
 
