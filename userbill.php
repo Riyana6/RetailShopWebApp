2d8bffe0be511206253ee5878952fd5135599204
JMJ Riyana <jeemariyana@gmail.com>
2020-04-10 23:55:36 +0530

    code

--- userbill.php
+++ userbill.php
@@ -40,7 +40,80 @@ if(isset($_GET['add']))
    			 $itemcode = "";
                 $unitprice = "";
                 $itemname  = "";
-		}
+        }
+        
+
+
+        $(document).ready(function()){
+            $(document).on('click', '#checkAll', function() {          	
+                $(".itemRow").prop("checked", this.checked);
+            });	
+            $(document).on('click', '.itemRow', function() {  	
+                if ($('.itemRow:checked').length == $('.itemRow').length) {
+                    $('#checkAll').prop('checked', true);
+                } else {
+                    $('#checkAll').prop('checked', false);
+                }
+            });  
+            var count = $(".itemRow").length;
+            $(document).on('click', '#addRows', function() { 
+                count++;
+                var htmlRows = '';
+                htmlRows += '<tr>';
+                htmlRows += '<td><input class="itemRow" type="checkbox"></td>';          
+                htmlRows += '<td><input type="text" name="productCode[]" id="productCode_'+count+'" class="form-control" autocomplete="off"></td>';          
+                htmlRows += '<td><input type="text" name="productName[]" id="productName_'+count+'" class="form-control" autocomplete="off"></td>';	
+                htmlRows += '<td><input type="number" name="quantity[]" id="quantity_'+count+'" class="form-control quantity" autocomplete="off"></td>';   		
+                htmlRows += '<td><input type="number" name="price[]" id="price_'+count+'" class="form-control price" autocomplete="off"></td>';		 
+                htmlRows += '<td><input type="number" name="total[]" id="total_'+count+'" class="form-control total" autocomplete="off"></td>';          
+                htmlRows += '</tr>';
+                $('#invoiceItem').append(htmlRows);
+            }); 
+            $(document).on('click', '#removeRows', function(){
+                $(".itemRow:checked").each(function() {
+                    $(this).closest('tr').remove();
+                });
+                $('#checkAll').prop('checked', false);
+                calculateTotal();
+            });		
+            $(document).on('blur', "[id^=quantity_]", function(){
+                calculateTotal();
+            });	
+            $(document).on('blur', "[id^=price_]", function(){
+                calculateTotal();
+            });	
+            $(document).on('blur', "#taxRate", function(){		
+                calculateTotal();
+            });	
+            $(document).on('blur', "#amountPaid", function(){
+                var amountPaid = $(this).val();
+                var totalAftertax = $('#totalAftertax').val();	
+                if(amountPaid && totalAftertax) {
+                    totalAftertax = totalAftertax-amountPaid;			
+                    $('#amountDue').val(totalAftertax);
+                } else {
+                    $('#amountDue').val(totalAftertax);
+                }	
+            });	
+            $(document).on('click', '.deleteInvoice', function(){
+                var id = $(this).attr("id");
+                if(confirm("Are you sure you want to remove this?")){
+                    $.ajax({
+                        url:"action.php",
+                        method:"POST",
+                        dataType: "json",
+                        data:{id:id, action:'delete_invoice'},				
+                        success:function(response) {
+                            if(response.status == 1) {
+                                $('#'+id).closest("tr").remove();
+                            }
+                        }
+                    });
+                } else {
+                    return false;
+                }
+            });
+        });
 ?>
 
 <!DOCTYPE html>
@@ -72,7 +145,7 @@ if(isset($_GET['add']))
             </div>
         </div>
 
-        
+
 
 
 
@@ -117,9 +190,8 @@ if(isset($_GET['add']))
                         <div class="row">
                             <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
                                 <button class="btn btn-danger " id="remove" type="submit">Delete</button>
-                                <button class="btn btn-success" id="add" name="add" type="submit"
-                                    >Add</button>
-                                    <button class="btn btn-success" id="newrow" name="newrow" type="submit"
+                                <button class="btn btn-success" id="add" name="add" type="submit">Add</button>
+                                <button class="btn btn-success" id="newrow" name="newrow" type="submit"
                                     onclick="myFunction()">New Row</button>
                             </div>
                         </div>
